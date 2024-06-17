ef254e020c9e88de4715ce119498d1ecfde9320a
Dot99 <gui.rocha.felix@gmail.com>
2024-06-17 12:36:23 +0100

    Lil fix h

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: gude-jes <gude-jes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 08:45:53 by gude-jes          #+#    #+#             */
-/*   Updated: 2024/06/17 08:43:04 by gude-jes         ###   ########.fr       */
+/*   Updated: 2024/06/17 12:36:04 by gude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,6 @@ typedef struct s_stack
 	int	*storage;
 	int	top;
 }				t_stack;
-
-// Counting sort struct -------
-typedef struct s_count_data
-{
-	size_t	*count;
-	int		*output;
-	int		min;
-}	t_count_data;
 
 # define BOTTOM 0
 
@@ -126,7 +118,6 @@ int		func_num_elem(char **argv);
 
 //Algos
 void	specific(t_stack *a, t_stack *b);
-//void	counting_sort(int *arr, int size);
 void	insertion_sort(int *storage, int chunk_size);
 void	radix_sort(t_stack *a, t_stack *b);
 #endif
